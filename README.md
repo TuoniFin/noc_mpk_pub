8247241a385342e7864084b26c4a1465b7316733
Hannu Balk <hannu@balk.fi>
2023-07-24 14:05:34 +0300

    Initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pilvilinna
+
+Pilvilinna on haavoittuva ohjelmisto opetustarkoituksiin. Älä käytä oikeissa ympäristöissä!
+
+## Asentaminen
+```bash
+git clone https://github.com/Rscl/pilvilinna.git && cd pilvilinna && bash setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Asenna Apache2 ja PHP
+sudo apt-get update
+sudo apt-get install -y apache2 php libapache2-mod-php php-mysql
+
+# Asenna Composer
+sudo apt-get install -y curl php-cli php-mbstring git unzip
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+# Asenna Node.js ja npm Pilvilinna Portaalin kääntämiseksi
+sudo snap install node --classic
+
+# Hanki Pilvilinna API ja Portaali
+git clone https://github.com/Rscl/pilvilinna-api.git
+git clone https://github.com/Rscl/pilvilinna-portal.git
+
+# Siirrä projektit /var/www -kansioon ja asenna riippuvuudet
+sudo mv pilvilinna-api /var/www/api
+sudo mv pilvilinna-portal /var/www/portal
+
+cd /var/www/api
+sudo composer install
+
+cd /var/www/portal
+sudo npm install
+
+# Käännä Pilvilinna Portaali
+sudo npm run build
+
+# Kopioi Apache2 konfiguraatiot
+sudo cp ./Apache2/pilvilinna-api.conf /etc/apache2/sites-available/
+sudo cp ./Apache2/pilvilinna-portal.conf /etc/apache2/sites-available/
+
+# Ota uudet sivustot käyttöön ja käynnistä Apache2 uudelleen
+sudo a2ensite pilvilinna-api
+sudo a2ensite pilvilinna-portal
+sudo systemctl restart apache2
+
+echo "Asennus on valmis!"
